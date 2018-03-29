f5f38605500450e877dba4c32695c72f877a8aa5
Jeremy Lacomis <jlacomis@cmu.edu>
2018-03-29 10:53:49 -0400

    Remove CSI hackery

--- code/gcd/compile.sh
+++ code/gcd/compile.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-clearsc() {
-    HOME=$(tput cup 0 0)
-    ED=$(tput ed)
-    EL=$(tput el)
-    ROWS=$(tput lines)
-    COLS=$(tput cols)
-    printf '%s%s' "$HOME" "$ED"
-    CMD="$@"
-    ${SHELL:=sh} -c "$CMD" | head -n $ROWS | while IFS= read LINE; do
-        printf '%-*.*s%s\n' $COLS $COLS "$LINE" "$EL"
-    done
-    printf '%s%s' "$ED" "$HOME"
-}
-
 COBOL=cobc
 COBOLFLAGS="-x"
 
@@ -23,10 +9,5 @@ if [ $# -lt 2 ] ; then
 fi
 
 src=`dirname $1`
-clearsc echo ""
-echo "$src:"
-echo "----------------------------------------"
-colordiff gcd.cobol "$src"/gcd.cobol -U100 | tail -n+4
-echo "----------------------------------------"
-$COBOL ${COBOLFLAGS} -o "$2" "$src"/gcd.cobol &>/dev/null
+$COBOL ${COBOLFLAGS} -o "$2" "$src"/gcd.cobol
 exit $?
